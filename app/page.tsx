5c1ae9867431ba6d3f458e2c92657cece8823214
Nubookd <trewqtrewqtsop@gmail.com>
2026-03-02 14:21:13 +0300

    fix page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-import RoutesMap from "@/components/features/RouteMap";
+import RoutesMap from "@/components/features/RoutesMap";
 
 export default function Page() {
-  return (
-    <RoutesMap
-      destinations={[
-        { id: 1, coords: [37.831, 55.936] },
-        { id: 2, coords: [37.871, 55.936] },
-        { id: 3, coords: [37.851, 55.916] },
-        { id: 4, coords: [37.851, 55.956] },
-      ]}
-    />
-  );
+  return <RoutesMap />;
 }
